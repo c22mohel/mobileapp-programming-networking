484b13c2f3aa24c47edd78cfdcd7ac3ea5506869
c22mohel <c22mohel@student.his.se>
2023-04-27 12:02:53 +0200

    skapade en ny adapter.java och länkade till mainactivity

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,12 +5,16 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
+    private ArrayList<RecyclerViewItem> mountain;
+    private adapter adapters;
 
 
     @Override
--- app/src/main/java/com/example/networking/RecyclerViewItem.java
+++ app/src/main/java/com/example/networking/RecyclerViewItem.java
@@ -1,4 +1,30 @@
 package com.example.networking;
 
 public class RecyclerViewItem {
+
+    public RecyclerViewItem(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    private String name;
+    private int size;
+
+
 }
